44cb5da5eceb4c90aca25e53e480f21a11ede606
LanceScheffer <lancescheffer@gmail.com>
2022-10-17 21:36:35 -0500

    Initial commit

---
+++ src/app/book-form-template/book-form-template.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-book-form-template',
+  templateUrl: './book-form-template.component.html',
+  styleUrls: ['./book-form-template.component.css']
+})
+export class BookFormTemplateComponent implements OnInit {
+  formHasBeenSubmitted = false;
+  bookDetails = {
+    title: "",
+    author: "",
+    genre: ""
+  };
+
+  onFormSubmit(formObj: NgForm) {
+    console.log('Submitted!');
+
+    this.formHasBeenSubmitted = true;
+
+    this.bookDetails.title = formObj.value.title;
+    this.bookDetails.author = formObj.value.author;
+    this.bookDetails.genre = formObj.value.genre;
+
+    formObj.reset();
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
