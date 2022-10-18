90b64492a0b5765aafec26582ce3519fc8f0dfcc
LanceScheffer <lancescheffer@gmail.com>
2022-10-18 16:37:42 -0500

    reactive approach

--- src/app/book-form-reactive/book-form-reactive.component.ts
+++ src/app/book-form-reactive/book-form-reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-book-form-reactive',
@@ -6,7 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-form-reactive.component.css'],
 })
 export class BookFormReactiveComponent implements OnInit {
+  reactiveForm: FormGroup;
+  formHasBeenSubmitted = false;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.reactiveForm = new FormGroup({
+      title: new FormControl(null, Validators.required),
+      author: new FormControl(null, Validators.required),
+      genre: new FormControl("mystery", Validators.required),
+    });
+  }
+  onFormSubmit() {
+    console.log('Submitted', this.reactiveForm);
+
+     // Change boolean form submitted variable to true
+     this.formHasBeenSubmitted = true;
+
+     // Reset Form after 5 seconds
+    setTimeout(() => {
+      this.reactiveForm.reset();
+      this.formHasBeenSubmitted = false;
+    }, 5000);
+
+  }
 }
